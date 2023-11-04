5f073573918050744f2972a48d4f58d265db51a7
pletzjd <j.pletzer17@gmail.com>
2023-11-03 21:06:18 -0400

    10-again functioning for when rote is false

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,7 @@
 function roll(dice, rote, again, wp){
     let roll = [];
     let roteReRoll = [];
+    let tenReRoll = [];
 
     let successes = 0;
     let fails = 0;
@@ -49,10 +50,44 @@ function roll(dice, rote, again, wp){
             roteTen++;
           }
         }
+        console.log('rote: '+ roteReRoll +' rote tens: '+ roteTen +' rote nines: '+ roteNine +' rote eights: '+ roteEight )
+      }
+
+      if(again === 10 && ten){
+        let tenCurrent = ten
+        let tenNew = 0
+        let loopCounter = -1
+        let tenReRollRow = []
+        while(tenCurrent){
+          loopCounter++
+          for(let k=0;k<tenCurrent;k++){
+            tenReRollRow[k]=Math.ceil(Math.random()*10);
+            if(tenReRollRow[k]<=7){
+              fails++;
+            }else if (tenReRollRow[k]===8){
+              successes++;
+              eight++;
+            }else if(tenReRollRow[k]===9){
+              successes++;
+              nine++;
+            }else{
+              successes++;
+              ten++;
+              tenNew++
+            }
+            console.log('row at '+ loopCounter + ': '+ tenReRollRow)
+          }
+          tenReRoll[loopCounter] = tenReRollRow;
+          tenReRollRow = [];
+          tenCurrent = tenNew;
+          tenNew = 0;
+        }
+        console.log('10-Agains: '+ tenReRoll+ ' Number of 10 again procs: '+ loopCounter+1)
       }
       
       console.log('roll: '+ roll +' successes: '+ successes +' fails: '+ fails +' tens: '+ ten +' nines: '+ nine +' eights: '+ eight)
-      console.log('rote: '+ roteReRoll +' rote tens: '+ roteTen +' rote nines: '+ roteNine +' rote eights: '+ roteEight )
+
+
   }
 
-roll(10, true, false, false)
+roll(10, false, 10, false)
