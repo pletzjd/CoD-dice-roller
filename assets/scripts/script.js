24067ae2dbacd4146b7b3515ee2eed1b7affd3ba
pletzjd <j.pletzer17@gmail.com>
2023-11-05 01:22:25 -0400

    Now counts number of successes. Added if statement to handle the rare case of the roll being zero.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
 function roll(dice, wp, rote, again){
   let roll = []; //blanc array for main roll
+  let successes = 0;
   console.log('roll: '+roll)
 
   if (wp) {
@@ -9,37 +10,47 @@ function roll(dice, wp, rote, again){
   for (let i = 0; i < dice; i++) {
     
     roll.push(Math.ceil(Math.random() * 10));
+
+    if(roll[roll.length-1] === 0){
+      roll[roll.lenght-1] = 10;
+    }
+    
     console.log('roll: '+roll)
   
     if (roll[roll.length-1] <= 7) {
       if(rote){
         console.log('rote')
-        roll = roll.concat(reroller(again))
+        roll = roll.concat(reroller(again));
         console.log('roll: '+roll)
       }
     } else if (roll[roll.length-1] === 8) {
       if(again === 8){
-        roll = roll.concat(reroller(again))
+        roll = roll.concat(reroller(again));
       }
       
     } else if (roll[roll.length-1] === 9) {
-      if(8<=again && again<=9){
+      if(8 <= again && again <= 9){
         roll = roll.concat(reroller(again))
       }
     } else {
-      if(8<=again){
+      if(8 <= again){
         roll = roll.concat(reroller(again))
       }
     }
   }
-console.log('final roll: '+ roll)
-console.log(roll)
+console.log('result: '+ roll)
+roll.forEach(x => { if( x >=8 ){ successes++ } })
+console.log('successes: '+ successes)
 }
 
 function reroller(again){
   let reRoll = []
   
   reRoll[0] = Math.ceil(Math.random() * 10);
+
+  if(reRoll[reRoll.length-1] === 0){
+    reRoll[reRoll.lenght-1] = 10;
+  }
 
   if(reRoll[0] >= again && again){
     let recall = reroller(again);
@@ -49,4 +60,4 @@ function reroller(again){
   return reRoll
 }
 
-roll(5,false,false,8)
+roll(5,false,true,10)
