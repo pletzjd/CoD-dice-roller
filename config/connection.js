c51b817bd1906e0e8c067ca46835b121ffabeef7
pletzjd <j.pletzer17@gmail.com>
2023-12-13 04:40:07 -0500

    Fixed spelling error in index.html. Adjusted button width so that initiative would not get split into two lines in firefox. Changed all colors to variables in style.css so that they can changed more easily later.

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,35 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-          rejectUnauthorized: true,        
+let sequelize;
+
+if(process.env.DB_HOST){
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,        
+        }
       }
     }
-  }
-);
+  );
+}else{
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+    }
+  );
+}
+
+
 
 module.exports = sequelize;
