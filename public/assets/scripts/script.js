4ead893857517d649e730a0807d35316563f8187
pletzjd <j.pletzer17@gmail.com>
2023-12-20 14:22:15 -0500

    Refactored and added comments to script.js. Changed styling of elemtents witht he class againReroll as bolder was no longer seemed to affect the font weight noticeably with recent styling changes.

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,64 +1,72 @@
+// Functions
+// Rolls a sigle die and determines what the result means in the context of a normal roll
 function roll(dice, wp, rote, again) {
-  let roll = []; //blanc array for main roll
+  let roll = [];
   let successes = 0;
 
+  //Adds three dice to the roll if willpower was used
   if (wp) {
     dice = dice + 3;
   }
 
+  // Rolls all the dice
   for (let i = 0; i < dice; i++) {
 
     roll.push(singleRoll());
 
-    if (roll[roll.length - 1] === 8 && again === 8) {
+    // Handles exploding dice rerolls and rote action rerolls
+    if (roll[roll.length - 1] >= again && again) {
       roll = roll.concat(reroller(again));
-    } else if (roll[roll.length - 1] === 9 && 8 <= again && again <= 9) {
-      roll = roll.concat(reroller(again))
-    } else if (roll[roll.length - 1] === 10 && 8 <= again) {
-      roll = roll.concat(reroller(again))
     }else if (roll[roll.length - 1] <= 7 && rote) {
       roll = roll.concat(reroller(again));
     }
-
   }
 
+  // Counts the number of successes from the roll
   roll.forEach(x => { if (x >= 8) { successes++ } })
 
   return [roll, successes];
 }
 
+// Handles exploding dice rerolls
 function reroller(again) {
   let reRoll = []
 
   reRoll[0] = singleRoll();
 
+  // Continues rerolling if new reroll triggers exploding dice
   if (reRoll[0] >= again && again) {
     let recall = reroller(again);
-    reRoll = reRoll.concat(recall);
+    reroll = reRoll.concat(recall);
   }
 
   return reRoll
 }
 
+// Rolls a sigle die and determines what the result means in the context of a chance die roll
 function chanceRoll() {
   let roll = singleRoll();
   let result;
 
+  // Determines meaning of chance die roll
   if (roll === 10) {
-    result = 'success';
+    result = 'Success';
   } else if (roll <= 9 && roll >= 2) {
-    result = 'fail';
+    result = 'Fail';
   } else {
-    result = 'dramatic fail';
+    result = 'Dramatic Fail';
   }
 
   return [roll, result];
 
 }
 
+// Rolls a single 10 sided dice
 function singleRoll() {
+  // Rolls one die
   let roll = Math.ceil(Math.random() * 10);
 
+  // Sets a roll resulting in 0 to 10
   if (roll === 0) {
     roll = 10;
   }
@@ -66,145 +74,9 @@ function singleRoll() {
   return roll
 }
 
-let rollForm = document.getElementById('rollForm')
-
-rollForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let playerName = document.getElementById('playerName').value
-  let description = document.getElementById('rollDescription').value
-  let rollObj;
-
-
-  if (e.submitter.id === 'regRoll') {
-    let rollResult;
-    let dice = parseInt(document.getElementById('dice').value)
-
-    if (!playerName || !description || !dice || dice <= 0) {
-      alert('Be sure to fill in Player Name, Desctiption and Dice Pool for normal rolls');
-      return;
-    } else {
-      let again;
-      let agains = document.querySelectorAll('input[name="agains"]');
-      agains.forEach(x => { if (x.checked) { again = parseInt(x.value) } });
-      let rote = document.getElementById('rote').checked;
-      let wp = document.getElementById('willpower').checked;
-      let advanced = document.getElementById('advanced').checked;
-
-      // Checks if HTML of form elements was altered
-      if(typeof wp != 'boolean' || typeof rote != 'boolean' || (again != 0 && again != 10 && again != 9 && again != 8 || typeof dice != 'number' || isNaN(dice))){
-        return
-      }
-
-      if (advanced) {
-        let rollOne = roll(dice, wp, rote, again);
-        let rollTwo = roll(dice, wp, rote, again);
-
-        rollObj = {
-          rollType: 'reg',
-          playerName: playerName,
-          description: description,
-          dice: dice,
-          willpower: wp,
-          again: again,
-          rote: rote,
-          advanced: advanced,
-          rollOne: rollOne[0].toString(),
-          rollTwo: rollTwo[0].toString(),
-          rollOneSuccesses: rollOne[1].toString(),
-          rollTwoSuccesses: rollTwo[1]
-        }
-
-      } else {
-        rollResult = roll(dice, wp, rote, again);
-
-        rollObj = {
-          rollType: 'reg',
-          playerName: playerName,
-          description: description,
-          dice: dice,
-          willpower: wp,
-          again: again,
-          rote: rote,
-          advanced: advanced,
-          rollOne: rollResult[0].toString(),
-          rollTwo: null,
-          rollOneSuccesses: rollResult[1].toString(),
-          rollTwoSuccesses: null
-        }
-      }
-    }
-
-  } else if (e.submitter.id === 'chanceRoll') {
-
-    if (!playerName || !description) {
-      alert('Be sure to fill in Player Name and Desctiption for chance rolls');
-      return;
-    } else {
-      rollResult = chanceRoll();
-
-      rollObj = {
-        rollType: 'chance',
-        playerName: playerName,
-        description: description,
-        dice: 1,
-        willpower: false,
-        again: 0,
-        rote: false,
-        advanced: false,
-        rollOne: rollResult[0].toString(),
-        rollTwo: null,
-        rollOneSuccesses: rollResult[1],
-        rollTwoSuccesses: null
-      }
-    }
-
-  } else if (e.submitter.id === 'initiativeRoll') {
-
-    if (!playerName) {
-      alert('Be sure to fill in Player Name for initiative rolls');
-      return;
-    } else {
-      description = 'Initiative';
-      rollResult = singleRoll();
-
-      rollObj = {
-        rollType: 'initiative',
-        playerName: playerName,
-        description: description,
-        dice: 1,
-        willpower: false,
-        again: 0,
-        rote: false,
-        advanced: false,
-        rollOne: rollResult.toString(),
-        rollTwo: null,
-        rollOneSuccesses: null,
-        rollTwoSuccesses: null
-      }
-    }
-  }
-
-  fetch('/api/roll', {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify(rollObj),
-  }).then(function (response) {
-    return response.json();
-  })
-  .then(function (){
-    location.assign(location.origin)
-  })
-
-})
-
+// Adds styling to each number in a roll to better visualize the significance of each number in the roll
 function rollParser(roll, type, again, rote){
+
   if(!again){
     again = 11;
   }
@@ -254,6 +126,7 @@ function rollParser(roll, type, again, rote){
     }
 
     rollElement.appendChild(spanElement)
+
     //Adds a , after each number if its not the last iteration
     if (index != arr.length - 1) {
       rollElement.innerHTML += ', '
@@ -263,6 +136,7 @@ function rollParser(roll, type, again, rote){
   return rollElement
 }
 
+// Creates one row of the roll table
 function tableRowMaker(rollObj) {
   //Creating all the elements to be added to the table
   let rollTable = document.getElementsByTagName('tbody')[0]
@@ -276,9 +150,11 @@ function tableRowMaker(rollObj) {
   let successesColumn = document.createElement('td');
 
   // Modifies content from roll object to be added as text content later
+  // Prepares Date Time column
   let timeStampContent = rollObj.createdAt.split('T');
   timeStampContent[1] = timeStampContent[1].split('.')[0];
 
+  //Prepares modifiers column
   let modifiersContent;
   if (rollObj.again === 10) {
     modifiersContent = '10-Again'
@@ -294,7 +170,6 @@ function tableRowMaker(rollObj) {
     }else(
       modifiersContent = 'Initiative Roll'
     )
-
   }
   if (rollObj.willpower) {
     modifiersContent = modifiersContent.concat(', Use Willpower')
@@ -324,8 +199,9 @@ function tableRowMaker(rollObj) {
   rollOneSuccessesElement.textContent = rollObj.rollOneSuccesses
   successesColumn.appendChild(rollOneSuccessesElement)
 
+  // Adds additonal elements to roll column and success column for advanced action rolls
   if (rollObj.advanced) {
-    //Additonal elements for advanced action
+    // Additonal elements for advanced action.
     let rollTwoRollElement = rollParser(rollObj.rollTwo, rollObj.rollType, rollObj.again, rollObj.rote)
     let rollTwoSuccessesElement = document.createElement('p');
     let rollSpacer = document.createElement('p');
@@ -350,7 +226,7 @@ function tableRowMaker(rollObj) {
 
 
 
-  //Append all the elements
+  //Append all the elements to table
   newRow.appendChild(timeStampColumn);
   newRow.appendChild(playerNameColumn);
   newRow.appendChild(rollDescriptionColumn);
@@ -359,10 +235,11 @@ function tableRowMaker(rollObj) {
   newRow.appendChild(rollColumn);
   newRow.appendChild(successesColumn);
   rollTable.appendChild(newRow)
-
 }
 
+// Populates the roll table and creates buttons for page navigation
 function init() {
+  // Determines the current page
   let queryString;
   if(!document.location.search){
     queryString = '?page=1'
@@ -370,6 +247,7 @@ function init() {
     queryString = document.location.search
   }
 
+  // Sets up page navigation
   fetch('/api/roll/pages')
   .then((function (response){
     return response.json()
@@ -383,17 +261,18 @@ function init() {
     let pageNumber = parseInt(document.location.search.split('page=')[1]) || 1
     let prevTabHRef
 
+    // Creates the previous page button. Sets location for the Previous button to bring the user too. Disables button if this is the first page
     if(pageNumber >=2 ){
       prevTabHRef = `?page=${pageNumber-1}`
     }else{
       prevTab.setAttribute('disabled', 'True')
       prevTabHRef = ''
     }
-
     prevTab.setAttribute('href', prevTabHRef)
     prevTab.setAttribute('class', 'tableNavButton')
     tableNav.appendChild(prevTab)
 
+    // Creates buttons to navigate to a specific page. Gives special styling to the button of the current page and disables it.
     for(let i =1; i <= pageTotal; i++){
       let newNavTab = document.createElement('button')
       newNavTab.innerHTML = i
@@ -407,6 +286,7 @@ function init() {
       tableNav.appendChild(newNavTab)
     }
 
+    // Creates the buttons to navigate to the next page and disables it if the current page is the last page.
     let nextTab = document.createElement('button')
     nextTab.innerHTML = 'NEXT'
     nextTab.setAttribute('class','tableNavButton')
@@ -419,7 +299,6 @@ function init() {
     }else {
       nextTabHRef = '?page=2'
     }
-
     nextTab.setAttribute('href', nextTabHRef)
     tableNav.appendChild(nextTab)
   })
@@ -427,6 +306,7 @@ function init() {
     console.log(err.message)
   })
 
+  // Gets the roll data for the table and then creates each row
   fetch(`/api/roll${queryString}`)
   .then(function (response) {
     return response.json()
@@ -443,6 +323,154 @@ function init() {
   })
 }
 
+// EVENT LISTENERS
+// Event listener for handling submission of the roll form for all the various roll types
+let rollForm = document.getElementById('rollForm')
+rollForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let rollObj;
+  // Gets information common to all roll types from the form
+  let playerName = document.getElementById('playerName').value
+  let description = document.getElementById('rollDescription').value
+
+  // Handles submission for the roll button
+  if (e.submitter.id === 'regRoll') {
+    let rollResult;
+    let dice = parseInt(document.getElementById('dice').value)
+
+    //check to make sure all necessary fields are filled in with proper data
+    if (!playerName || !description || !dice || dice <= 0) {
+      alert('Be sure to fill in Player Name, Desctiption and Dice Pool for normal rolls');
+      return;
+    } else {
+      // Gets information from form for a regular roll
+      let again;
+      let agains = document.querySelectorAll('input[name="agains"]');
+      agains.forEach(x => { if (x.checked) { again = parseInt(x.value) } });
+      let rote = document.getElementById('rote').checked;
+      let wp = document.getElementById('willpower').checked;
+      let advanced = document.getElementById('advanced').checked;
+
+      // Checks if HTML of form elements was altered
+      if(typeof wp != 'boolean' || typeof rote != 'boolean' || (again != 0 && again != 10 && again != 9 && again != 8 || typeof dice != 'number' || isNaN(dice))){
+        return
+      }
+
+      // Rolls dice and creates object to be sent in the body of the request to add this roll to the database
+      if (advanced) {
+        let rollOne = roll(dice, wp, rote, again);
+        let rollTwo = roll(dice, wp, rote, again);
+
+        rollObj = {
+          rollType: 'reg',
+          playerName: playerName,
+          description: description,
+          dice: dice,
+          willpower: wp,
+          again: again,
+          rote: rote,
+          advanced: advanced,
+          rollOne: rollOne[0].toString(),
+          rollTwo: rollTwo[0].toString(),
+          rollOneSuccesses: rollOne[1].toString(),
+          rollTwoSuccesses: rollTwo[1]
+        }
+      } else {
+        rollResult = roll(dice, wp, rote, again);
+
+        rollObj = {
+          rollType: 'reg',
+          playerName: playerName,
+          description: description,
+          dice: dice,
+          willpower: wp,
+          again: again,
+          rote: rote,
+          advanced: advanced,
+          rollOne: rollResult[0].toString(),
+          rollTwo: null,
+          rollOneSuccesses: rollResult[1].toString(),
+          rollTwoSuccesses: null
+        }
+      }
+    }
+
+    // Handles submission for the Chance Die button
+  } else if (e.submitter.id === 'chanceRoll') {
+
+    //check to make sure all necessary fields are filled in with proper data
+    if (!playerName || !description) {
+      alert('Be sure to fill in Player Name and Desctiption for chance rolls');
+      return;
+    } else {
+      // Rolls dice and creates object to be sent in the body of the request to add this roll to the database
+      rollResult = chanceRoll();
+
+      rollObj = {
+        rollType: 'chance',
+        playerName: playerName,
+        description: description,
+        dice: 1,
+        willpower: false,
+        again: 0,
+        rote: false,
+        advanced: false,
+        rollOne: rollResult[0].toString(),
+        rollTwo: null,
+        rollOneSuccesses: rollResult[1],
+        rollTwoSuccesses: null
+      }
+    }
+    // Handles submission for the Initiative button
+  } else if (e.submitter.id === 'initiativeRoll') {
+    //check to make sure all necessary fields are filled in with proper data
+    if (!playerName) {
+      alert('Be sure to fill in Player Name for initiative rolls');
+      return;
+    } else {
+      // Rolls dice and creates object to be sent in the body of the request to add this roll to the database
+      description = 'Initiative';
+      rollResult = singleRoll();
+
+      rollObj = {
+        rollType: 'initiative',
+        playerName: playerName,
+        description: description,
+        dice: 1,
+        willpower: false,
+        again: 0,
+        rote: false,
+        advanced: false,
+        rollOne: rollResult.toString(),
+        rollTwo: null,
+        rollOneSuccesses: null,
+        rollTwoSuccesses: null
+      }
+    }
+  }
+
+  // Adds new roll to database and refreshes the page so it appears in the table
+  fetch('/api/roll', {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(rollObj),
+  }).then(function (response) {
+    return response.json();
+  })
+  .then(function (){
+    location.assign(location.origin)
+  })
+
+})
+
+//Event listener to navigate the different pages of the roll table
 let tableNav = document.getElementById('tableNav')
 tableNav.addEventListener('click', (e) => {
   if(e.target.getAttribute('href')){
@@ -450,4 +478,5 @@ tableNav.addEventListener('click', (e) => {
   }
 })
 
+// Page load function call
 init()
